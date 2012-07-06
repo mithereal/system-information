40f9d34ce7f380d4c25547d36fe88cfd5c9d3199
A. Qahar Mudzakkir <qahar.5010@gmail.com>
2012-07-06 18:54:59 +0700

    Upload v1.0.0

---
+++ admin/controller/tool/system_info.php
@@ -0,0 +1,98 @@
+<?php
+class ControllerToolSystemInfo extends Controller {
+	public function index() {
+		$this->data = $this->load->language('tool/system_info');
+		
+		$this->document->setTitle($this->data['heading_title']);
+		$this->document->addStyle('view/stylesheet/systeminfo.css');
+		
+		$this->data['breadcrumbs'] = array();
+		$this->data['breadcrumbs'][] = array(
+			'text'      => $this->language->get('text_home'),
+			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+			'separator' => false
+		);
+		$this->data['breadcrumbs'][] = array(
+			'text'      => $this->language->get('heading_title'),
+			'href'      => $this->url->link('tool/system_info', 'token=' . $this->session->data['token'], 'SSL'),
+			'separator' => ' <span class="separator">&#187;</span> '
+		);
+		
+		/* $this->load->model('setting/store');
+		
+		$settings = array(
+			'config_name',
+			'config_owner',
+			'config_email',
+			'config_title',
+			'config_seo_url',
+			'config_template',
+			'config_language',
+			'config_currency',
+			'config_currency_auto',
+			'config_guest_checkout',
+			'config_invoice_prefix',
+			'config_mail_protocol',
+			'config_alert_mail',
+			'config_account_mail'
+		);
+		foreach ($settings as $setting) {
+				$this->data[$setting] = $this->config->get($setting);
+		} */
+		
+		$this->data['token'] = $this->session->data['token'];
+		
+		$this->template = 'tool/system_info.tpl';
+		$this->children = array(
+			'common/header',	
+			'common/footer'	
+		);
+		
+		$this->response->setOutput($this->render());
+	}
+	
+	public function version() {
+		$this->data = $this->load->language('tool/system_info');
+		$currentVersion = $this->language->get('product_version');
+		
+		if (isset($this->request->get['product'])) { $prodName = $this->request->get['product']; } else { $prodName = ''; }
+		$checkVersion = 'http://www.echothemes.com/assets/version/' . $prodName . '.xml';
+		
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $checkVersion);
+		curl_setopt($curl, CURLOPT_HEADER, 0);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$returnData = curl_exec($curl);
+		$returnCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		curl_close($curl);
+		
+		if ($returnCode == 200) { // Succeeded
+			$data = simplexml_load_string($returnData);
+			if ($data->version == $currentVersion || $data->version <= $currentVersion) {
+				$statusClass	= 'green';
+				$statusText 	= $this->language->get('text_noupdate');
+				$changelogDesc = '';
+			} elseif ($data->version >= $currentVersion) {
+				$statusClass	= 'orange';
+				$statusText 	= $this->language->get('text_update');
+				$changelogDesc = $data->changelog;
+			} else {
+				$statusClass	= 'red';
+				$statusText		= $this->language->get('text_missupdate');
+				$changelogDesc = '';
+			}
+		} else { 
+			$statusClass	= 'red';
+			$statusText 	= $this->language->get('text_failconnect');
+			$changelogDesc = '';
+		}
+
+		$output = '<span class="version ' . $statusClass . '">' . $statusText . '</span>';
+		if ($changelogDesc) {
+			$output .= '<div id="updateChangelog">' . $changelogDesc . '</div>';
+		}
+
+		$this->response->setOutput(json_encode($output));
+	}
+}
+?>
---
+++ admin/language/english/tool/system_info.php
@@ -0,0 +1,62 @@
+<?php
+//=========== Heading
+$_['heading_title']			= 'System Information';
+
+//=========== Menu
+$_['menu_system_info']  	= 'System Info';
+$_['menu_php_settings']  	= 'PHP Settings';
+$_['menu_dir_permission']  = 'Dir Permission';
+$_['menu_about']  			= 'About';
+
+//=========== Text
+$_['text_system_info']		= 'System Information';
+$_['text_oc_version']		= 'Opencart Version';
+$_['text_operating_sys']	= 'Operating System';
+$_['text_web_server']		= 'Web Server';
+$_['text_web_host']			= 'Web Host Info';
+$_['text_mysql_version']	= 'MySQL Server Version';
+$_['text_mysql_host']		= 'MySQL Host Info';
+$_['text_root_path']			= 'Root Path';
+
+$_['text_php_version']		= 'PHP Version';
+$_['text_req_php_version']	= '>= 5.0';
+$_['text_register_globals']= 'Register Globals';
+$_['text_magic_quotes']		= 'Magic Quotes GPC';
+$_['text_file_uploads']		= 'File Uploads';
+$_['text_session_start']	= 'Session Auto Start';
+
+$_['text_mysql']				= 'MySQL';
+$_['text_gd']					= 'GD';
+$_['text_curl']				= 'Curl';
+$_['text_fsock']				= 'Fsock';
+$_['text_zip']					= 'Zip';
+$_['text_xml']					= 'Xml';
+
+$_['text_on']					= 'On';
+$_['text_off']					= 'Off';
+$_['text_value']				= 'Value';
+$_['text_server']				= 'Server';
+$_['text_status']				= 'Status';
+$_['text_setting']			= 'Setting';
+$_['text_required']			= 'Required';
+$_['text_current']			= 'Current';
+$_['text_extenisons']		= 'Extensions';
+$_['text_directory']			= 'Directory';
+$_['text_permission']		= 'Permission';
+
+$_['text_name']				= 'Product Name';
+$_['text_version']			= 'Version';
+$_['text_author']				= 'Author';
+$_['text_author_url']		= 'Author URL';
+$_['text_documentation']	= 'Documentation';
+$_['text_support']			= 'Support';
+$_['text_ticket_support']	= 'Ticket Support';
+
+//== App Info
+$_['product_version']		= '1.0.0';
+$_['text_check_version']	= 'Check Version';
+$_['text_noupdate']			= 'Up to date!';
+$_['text_update']				= '<blink>New update available!</blink>';
+$_['text_missupdate']		= 'Unable to perform version check!';
+$_['text_failupdate']		= 'Unable connect to update server!';
+?>
