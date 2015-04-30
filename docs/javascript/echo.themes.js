a7e59f1ba9a9d0eb5cd2149598fbdf6652b31f9d
A. Qahar Mudzakkir <qahar@octave-tech.com>
2015-05-01 01:28:58 +0700

    update docs

--- docs/javascript/echo.themes.js
+++ docs/javascript/echo.themes.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	SyntaxHighlighter.defaults['toolbar'] = false;
+	SyntaxHighlighter.all();
+		
 	$('#infoTabs ul').idTabs('infoTab1',function(id,list,set){ 
 		$("a",set).removeClass("selected").filter("a[href='"+id+"']").addClass("selected"); 
 		for(i in list)$(list[i]).hide();
